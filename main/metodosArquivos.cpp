6df5e490d49f64050d8d55241cb27633b6db44e4
murilofsilva <murilofsilva11@gmail.com>
2022-11-14 22:08:38 -0400

    uiuiui nem cavalo aguenta

--- main/metodosArquivos.cpp
+++ main/metodosArquivos.cpp
@@ -52,7 +52,7 @@ Candidato* criarVetorCandidato(Candidato *lista, int tamanhoLista)
 void gerarArquivoFinal(Curso **cursosOrdenados, int quantidadeCursos, Candidato *listaCadidatos)
 {
     FILE *arquivo;
-    arquivo = fopen("teste.txt", "w");
+    arquivo = fopen("teste.txt", "w+");
     
     if (!arquivo)
     {
@@ -74,13 +74,30 @@ void gerarArquivoFinal(Curso **cursosOrdenados, int quantidadeCursos, Candidato 
         for (int candidato = 0; candidato < quantidadeCandidatosCurso; candidato++)
             vetorCandidatosOrdenados[candidato] = vetorCandidatos + candidato;
 
-        ordenarCandidatosPorCota(0, quantidadeCandidatosCurso - 1, vetorCandidatosOrdenados);
-        ordenarCandidatosPorNota(0, quantidadeCandidatosCurso, vetorCandidatosOrdenados);
+        // ordenarCandidatosPorCota(0, quantidadeCandidatosCurso - 1, vetorCandidatosOrdenados);
+        // ordenarCandidatosPorNota(0, quantidadeCandidatosCurso, vetorCandidatosOrdenados);
+
+        Candidato *candidato = listaCadidatos;
+        if (candidato != NULL)
+        {
+            do
+            {
+                for (int i = 0; i < quantidadeCandidatosCurso; i++)
+                {
+                    if (vetorCandidatosOrdenados[i]->inscricao == candidato->inscricao)
+                        vetorCandidatosOrdenados[i]->inscricao = candidato->inscricao;
+                }
+                candidato = candidato->prox;
+            } while (candidato != NULL && candidato->prox != NULL);
+            
+        }
 
         for (int candidato = 0; candidato < quantidadeCandidatosCurso; candidato++)
-            fprintf(arquivo, "%d %s %.2f\n", vetorCandidatosOrdenados[candidato]->inscricao,
-                    vetorCandidatosOrdenados[candidato]->tipoVaga, vetorCandidatosOrdenados[candidato]->notaFinal);
-
+            fprintf(arquivo, "%d %f %f %f %f %f %s %.2f\n", vetorCandidatosOrdenados[candidato]->inscricao,
+                    vetorCandidatosOrdenados[candidato]->escoreLin, vetorCandidatosOrdenados[candidato]->escoreMat,
+                    vetorCandidatosOrdenados[candidato]->escoreNat, vetorCandidatosOrdenados[candidato]->escoreHum,
+                    vetorCandidatosOrdenados[candidato]->escoreRed, vetorCandidatosOrdenados[candidato]->tipoVaga,
+                    vetorCandidatosOrdenados[candidato]->notaFinal);
         i++;
     }
 
